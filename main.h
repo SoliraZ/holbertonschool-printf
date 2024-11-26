4649ba4ae143a21065f95e01071bab15378fe46c
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-26 15:18:11 +0100

    added structure for all print function, prototype and all the include

--- main.h
+++ main.h
@@ -1,6 +1,23 @@
-#ifndef PRINTF
-#define PRINTF
+#ifndef MAIN_H
+#define MAIN_H
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct types
+{
+	char *type;
+	int (*f)(va_list);
+} types_t;
+
+int	_printf(const char *format, ...);
+int	_format(char type, va_list args);
+int	_putchar(char c);
+int	print_char(va_list args);
+int	print_string(va_list args);
+int	print_percent(va_list args);
+int	print_int(va_list args);
 
 #endif
