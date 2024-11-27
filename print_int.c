c28c9873728bda228d1e1af0197d1097b6a9614e
danish872 <danishimwe872@gmail.com>
2024-11-27 17:36:03 +0000

    print_integer_modified_the_point_of_the_positive_case

---
+++ print_int.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *  * print_int - Function that print an int.
+ *   * @args: Character to print.
+ *    *
+ *     * Return: Number of characters printed.
+ *      */
+
+int     print_int(va_list args)
+{
+	int     nb = va_arg(args, int);
+	int     length = 0;
+	int     power = 1;
+	int     div;
+
+	if (nb < 0)
+	{
+		length = length + 1;
+		_putchar('-');
+		nb = -nb;
+	}
+	if (nb < 10)
+		return (length = length + _putchar(nb + '0'));
+	div = nb;
+	while (div >= 10)
+	{
+		power = power * 10;
+		div = div / 10;
+	}
+	while (power >= 1)
+	{
+		length = length + _putchar(((nb / power) % 10) + '0');
+		power = power / 10;
+	}
+	return (length);
+}
