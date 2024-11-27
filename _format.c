0b5efbdf1f49294bc3e414d5933b9aeda2fc914a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-27 18:14:29 +0100

    edited the _format.c to have a better comprehension and added the call to the function putchar if the type is not handled

--- _format.c
+++ _format.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _format - Function that print characters.
- * @type: character string.
+ * _format - Function that select the good function to use.
+ * @type: the type.
  * @args: arguments.
  *
  * Return: the number of characters printed.
@@ -11,20 +11,23 @@
 int	_format(const char type, va_list args)
 {
 	int	i;
-	types_t types[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_int},
-		{"i", print_int},
+	types_t format[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_int},
+		{'i', print_int},
 		{'\0', NULL}
 	};
 
 	i = 0;
-	while (types[i].type != '\0')
+	while (format[i].types)
 	{
-		if (types[i].type == type)
-			return (types[i].f(args));
+		if (type == format[i].types)
+			return (format[i].f(args));
 		i++;
 	}
+	_putchar('%');
+	_putchar(type);
+	return (2);
 }
