f4331d7dbacdadf362f5d39fccb015051e318ecd
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-27 18:16:12 +0100

    created _printf.c file and corrected betty

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _printf - Function that print characters.
+ * @format: character string.
+ *
+ * Return: the number of characters printed.
+ */
+
+int	_printf(const char *format, ...)
+{
+	int	i;
+	int	length;
+	va_list args;
+
+	va_start(args, format);
+
+	i = 0;
+	length = 0;
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			length += _format(format[i + 1], args);
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			length++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (length);
+}
